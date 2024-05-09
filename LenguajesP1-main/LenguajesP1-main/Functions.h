08903669989368ce807b4efa679f5b9facd90bb4
Josue <jecheverria@estudiantec.cr>
2024-05-08 21:40:55 -0600

    changes

--- LenguajesP1-main/LenguajesP1-main/Functions.h
+++ LenguajesP1-main/LenguajesP1-main/Functions.h
@@ -11,22 +11,6 @@ typedef struct
     char nombre[20];
 } TablaPeriodica;
 
-// Estructura de un elemento
-typedef struct
-{
-    double numeroAtomico;
-    int neutrones;
-} Elemento;
-
-// Estructura de las sumas de los logaritmos
-typedef struct
-{
-    double sumLogX;
-    double sumLogY;
-    double sumLogXY;
-    double sumLogX2;
-} LogSums;
-
 // Estructura de una tupla de doubles
 typedef struct
 {
@@ -44,72 +28,13 @@ void verTabla(Elemento elementos[])
     }
 }
 
-// Funcion que calcula las sumas de los logaritmos de los elementos
-LogSums calcular_sum_logs(Elemento elementos[])
-{
-    // Inicializar las sumas
-    LogSums sums = {0.0, 0.0, 0.0, 0.0};
 
-    // Calcular los logaritmos y las sumas
-    double logNumeroAtomico, logNeutrones;
-    for (int i = 0; i < 100; i++)
-    {
-        // Calcular los logaritmos
-        logNumeroAtomico = log(elementos[i].numeroAtomico);
-
-        // Sumar los logaritmos
-        sums.sumLogX += logNumeroAtomico;
-        sums.sumLogX2 += pow(logNumeroAtomico, 2);
-
-        // Si el elemento tiene neutrones, sumar el logaritmo de los neutrones y el producto de los logaritmos
-        if (elementos[i].neutrones != 0)
-        {
-            logNeutrones = log(elementos[i].neutrones);
-            sums.sumLogY += logNeutrones;
-            sums.sumLogXY += (logNeutrones * logNumeroAtomico);
-        }
-    }
-    return sums;
-}
-
-// Funcion que aplica la regla de Cramer para resolver un sistema de ecuaciones de 2x2
-Tupla_doubles aplicar_cramer(double ecuaciones[2][3])
-{
-    
-    double matriz_delta_K[2][2] = {{ecuaciones[0][1], ecuaciones[0][2]}, {ecuaciones[1][1], ecuaciones[1][2]}};
-
-    // Se calcula el determinate de matriz_delta_K 
-    double delta_K = matriz_delta_K[0][0] * matriz_delta_K[1][1] - matriz_delta_K[0][1] * matriz_delta_K[1][0];
-
-    double matriz_delta_B[2][2] = {{ecuaciones[0][0], ecuaciones[0][2]}, {ecuaciones[1][0], ecuaciones[1][2]}};
-
-    // Se calcula el determinate de matriz_delta_B 
-    double delta_B = matriz_delta_B[0][0] * matriz_delta_B[1][1] - matriz_delta_B[0][1] * matriz_delta_B[1][0];
-
-    double matriz_delta_S[2][2] = {{ecuaciones[0][0], ecuaciones[0][1]}, {ecuaciones[1][0], ecuaciones[1][1]}};
-
-    // Se calcula el determinate de matriz_delta_S 
-    double delta_S = matriz_delta_S[0][0] * matriz_delta_S[1][1] - matriz_delta_S[0][1] * matriz_delta_S[1][0];
-
-    double K = delta_K / delta_S;
-
-    double B = delta_B / delta_S;
-
-    Tupla_doubles KB = {-K, B};
-    return KB;
-}
-
-void print_formula(double a, double B)
-{
-    printf("La formula es: y = %f * x^%f\n", a, B);
-}
-
-// TODO: Se puede unir esta funcion con otra funcion
-double obtener_parametro_a(double K)
-{
-    double a = exp(K);
-    return a;
-}
+// // TODO: Se puede unir esta funcion con otra funcion
+// double obtener_parametro_a(double K)
+// {
+//     double a = exp(K);
+//     return a;
+// }
 
 // TODO: No es necesario hacer dos ciclos, se puede hacer en uno solo
 void generar_tabla_estimaciones(double a, double B, int estimaciones[100])
@@ -123,7 +48,7 @@ void generar_tabla_estimaciones(double a, double B, int estimaciones[100])
         estimaciones[i - 1] = estimacion;
     }
 }
-void desplegar_Tabla(TablaPeriodica elementos[100], Elemento lista[100], int estimaciones[100])
+void desplegar_Tabla(Elemento lista[100], int estimaciones[100])
 {
     for (int i = 0; i < 100; i++)
     {
---
+++ LenguajesP1-main/LenguajesP1-main/obtener_funcion.h
@@ -0,0 +1,109 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// Estructura de una tupla de doubles
+typedef struct
+{
+    double first_double;
+    double second_double;
+} Tupla_doubles;
+
+
+// Estructura de las sumas de los logaritmos
+typedef struct
+{
+    double sumLogX;
+    double sumLogY;
+    double sumLogXY;
+    double sumLogX2;
+} LogSums;
+
+
+// Estructura de un elemento
+typedef struct
+{
+    double numeroAtomico;
+    int neutrones;
+} Elemento;
+
+
+// Funcion que calcula las sumas de los logaritmos de los elementos
+LogSums calcular_sum_logs(Elemento elementos[])
+{
+    // Inicializar las sumas
+    LogSums sums = {0.0, 0.0, 0.0, 0.0};
+
+    // Calcular los logaritmos y las sumas
+    double logNumeroAtomico, logNeutrones;
+    for (int i = 0; i < 100; i++)
+    {
+        // Calcular los logaritmos
+        logNumeroAtomico = log(elementos[i].numeroAtomico);
+
+        // Sumar los logaritmos
+        sums.sumLogX += logNumeroAtomico;
+        sums.sumLogX2 += pow(logNumeroAtomico, 2);
+
+        // Si el elemento tiene neutrones, sumar el logaritmo de los neutrones y el producto de los logaritmos
+        if (elementos[i].neutrones != 0)
+        {
+            logNeutrones = log(elementos[i].neutrones);
+            sums.sumLogY += logNeutrones;
+            sums.sumLogXY += (logNeutrones * logNumeroAtomico);
+        }
+    }
+    return sums;
+}
+
+
+
+
+Tupla_doubles aplicar_cramer(double ecuaciones[2][3])
+{
+    
+    double matriz_delta_K[2][2] = {{ecuaciones[0][1], ecuaciones[0][2]}, {ecuaciones[1][1], ecuaciones[1][2]}};
+
+    // Se calcula el determinate de matriz_delta_K 
+    double delta_K = matriz_delta_K[0][0] * matriz_delta_K[1][1] - matriz_delta_K[0][1] * matriz_delta_K[1][0];
+
+    double matriz_delta_B[2][2] = {{ecuaciones[0][0], ecuaciones[0][2]}, {ecuaciones[1][0], ecuaciones[1][2]}};
+
+    // Se calcula el determinate de matriz_delta_B 
+    double delta_B = matriz_delta_B[0][0] * matriz_delta_B[1][1] - matriz_delta_B[0][1] * matriz_delta_B[1][0];
+
+    double matriz_delta_S[2][2] = {{ecuaciones[0][0], ecuaciones[0][1]}, {ecuaciones[1][0], ecuaciones[1][1]}};
+
+    // Se calcula el determinate de matriz_delta_S 
+    double delta_S = matriz_delta_S[0][0] * matriz_delta_S[1][1] - matriz_delta_S[0][1] * matriz_delta_S[1][0];
+
+    double K = delta_K / delta_S;
+
+    double B = delta_B / delta_S;
+
+    Tupla_doubles KB = {-K, B};
+    return KB;
+}
+
+Tupla_doubles obtener_funcion(Elemento elementos[]){
+
+    // Calcular las sumas de los logaritmos
+    LogSums sums = calcular_sum_logs(elementos);
+
+    // Crear la matriz de la formula
+    double formula1[2][3] = {{100, sums.sumLogX, sums.sumLogY}, {sums.sumLogX, sums.sumLogX2, sums.sumLogXY}};
+
+    Tupla_doubles KB = aplicar_cramer(formula1);
+
+    Tupla_doubles AB = {log(KB.first_double), KB.second_double};
+
+    return AB;
+}
+
+
+
+#endif // FUNCTIONS_H
